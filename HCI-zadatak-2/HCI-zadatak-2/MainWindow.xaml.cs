0ed4b7e23f52634953e27afffcb229f3b9f8545d
UrosOgrizovic <uros.ogrizovic671@gmail.com>
2019-05-07 14:36:57 +0200

    Initial commit

---
+++ HCI-zadatak-2/HCI-zadatak-2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace HCI_zadatak_2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		
+		public MainWindow()
+		{
+			InitializeComponent();
+			cityMap.Source = new BitmapImage(new Uri("/images/MapNS.png", UriKind.Relative));
+		}
+
+		
+	}
+}
